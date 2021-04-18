49f7fce64228a71657e056df7ee7fd764ad742f0
NotTacoz <tacozpvp@gmail.com>
2021-04-18 22:01:26 +0800

    🔨 | Fixed Several Problems with Firebase Hosting

--- src/index.js
+++ src/index.js
@@ -4,6 +4,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// eslint-disable-next-line no-unused-vars
+/*
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyCOETJBWJQ8dNQnJilsND5CoT79GBHKZUs',
+  authDomain: 'genshin-mains.firebaseapp.com',
+  databaseURL: 'https://genshin-mains-default-rtdb.firebaseio.com',
+  projectId: 'genshin-mains',
+  storageBucket: 'genshin-mains.appspot.com',
+  messagingSenderId: '82953203784',
+  appId: '1:82953203784:web:b825c166d63766c475287d',
+  measurementId: 'G-00DBVS0PXW'
+};
+
+firebase.initializeApp(config);
+*/
 
 ReactDOM.render(
   <React.StrictMode>
