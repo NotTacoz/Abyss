ec37e2f6042c527425180ae70145ebd14722a1f8
NotTacoz <tacozpvp@gmail.com>
2021-04-18 18:46:40 +0800

    🍓 Commit Update Fixes

---
+++ src/App.js
@@ -0,0 +1,28 @@
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable space-before-function-paren */
+/* eslint-disable semi */
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          piss and
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
