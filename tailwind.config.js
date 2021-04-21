629eaa7aa7ab1a90b340d0682d733021380c2bda
NotTacoz <tacozpvp@gmail.com>
2021-04-21 20:13:31 +0800

    ✨| Tailwind CSS Migration + Started Work on Layouts and Elements

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/* eslint-disable comma-dangle */
+// tailwind.config.js
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
