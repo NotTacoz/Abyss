d1b9f036350ab701ece18af6d5c4f53aa5b43803
NotTacoz <tacozpvp@gmail.com>
2021-04-30 17:07:30 +0800

    yeah yeah less sgoooo

---
+++ src/notFound.js
@@ -0,0 +1,20 @@
+import React from 'react'
+// eslint-disable-next-line no-unused-vars
+import { Link } from 'react-router-dom'
+
+class NotFoundPage extends React.Component {
+  render () {
+    return (
+      <div className="content">
+        <h2>404</h2>
+        <h1>Page Not Found</h1>
+        <p>You are a dissapointment.</p>
+        <h3>Why am you so stupid? Why did try breaking the website?</h3>
+        <p>because you are unable to actually manage anything with your monkey brain of yours. Go away and play with lego or something. I absolutely hate that you tried to find a way to break my website</p>
+        <h3>But if you misspelled</h3>
+        <p>thats ok! we all make mistakes :)</p>
+      </div>
+    )
+  }
+}
+export default NotFoundPage
