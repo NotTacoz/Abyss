cc25ea493351948ebc1079b9b59a368445ecf123
NotTacoz <tacozpvp@gmail.com>
2021-05-09 18:44:35 +0800

    Formatting

--- src/components/Account.js
+++ src/components/Account.js
@@ -13,15 +13,15 @@ import {
   IfFirebaseUnAuthed
 } from '@react-firebase/auth'
 const config = {
-  apiKey: 'AIzaSyCOETJBWJQ8dNQnJilsND5CoT79GBHKZUs',
-  authDomain: 'genshin-mains.firebaseapp.com',
-  databaseURL: 'https://genshin-mains-default-rtdb.firebaseio.com',
-  projectId: 'genshin-mains',
-  storageBucket: 'genshin-mains.appspot.com',
-  messagingSenderId: '82953203784',
-  appId: '1:82953203784:web:b825c166d63766c475287d',
-  measurementId: 'G-00DBVS0PXW'
-}
+  apiKey: "AIzaSyCOETJBWJQ8dNQnJilsND5CoT79GBHKZUs",
+  authDomain: "genshin-mains.firebaseapp.com",
+  databaseURL: "https://genshin-mains-default-rtdb.firebaseio.com",
+  projectId: "genshin-mains",
+  storageBucket: "genshin-mains.appspot.com",
+  messagingSenderId: "82953203784",
+  appId: "1:82953203784:web:b825c166d63766c475287d",
+  measurementId: "G-00DBVS0PXW",
+};
 
 function Account () {
   return (
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 /* eslint-disable semi */
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 // eslint-disable-next-line no-unused-vars
 /*
 import * as firebase from 'firebase';
@@ -29,8 +29,7 @@ ReactDOM.render(
       <App />
     </React.StrictMode>
   </BrowserRouter>,
-  document.getElementById('root')
-
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
