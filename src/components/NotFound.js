de4e7e722cc2519b9520cbf594ed58e66773eabe
NotTacoz <tacozpvp@gmail.com>
2021-05-10 18:03:48 +0800

    yeah

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -9,15 +9,20 @@ class NotFoundPage extends React.Component {
     return (
       <div className="content">
         <div className="pt-12">
-          <img
-            className="w-96 mx-auto"
-            src={NotFoundImg}
-            alt="facepalm"
-          />
+          <img className="w-96 mx-auto" src={NotFoundImg} alt="facepalm" />
           <h1 className="text-center pb-2">What. The. Actual. Hell.</h1>
-          <p className="text-center font-bold">Did you just try to break my website!!</p>
+          <p className="text-center font-bold">
+            Did you just try to break my website!!
+          </p>
           <h2 className="text-center">
-            I am disappointed. But perhaps, I am not surpised <a href="https://www.youtube.com/watch?time_continue=1&v=j5a0jTc9S10&feature=emb_title" target="_blank" rel="noreferrer">.</a>
+            I am disappointed. But perhaps, I am not surpised{" "}
+            <a
+              href="https://www.youtube.com/watch?time_continue=1&v=j5a0jTc9S10&feature=emb_title"
+              target="_blank"
+              rel="noreferrer"
+            >
+              .
+            </a>
           </h2>
         </div>
       </div>
