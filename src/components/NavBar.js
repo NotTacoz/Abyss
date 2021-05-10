5a2076381709358d26802c118d04285aa39c18c3
NotTacoz <tacozpvp@gmail.com>
2021-05-10 17:56:24 +0800

    404 remake

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,125 +7,127 @@ import { Route, NavLink } from "react-router-dom";
 import Home from "./../img/sidebarbuttons/Home.png";
 import Character from "./../img/sidebarbuttons/Character.png";
 
-function NavBar() {
-  return (
-    <div>
-      <div className="sidebar">
-        {/* <div className="sidebar-bg"></div> */}
-        <div className="text-center mt-10 mb-12 z-10">
-          <div className="inline-flex mb-8">
-            <h1 className="text-white">Abyss</h1>
-            <h1 className="text-red-500">Media</h1>
+class NavBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="sidebar">
+          {/* <div className="sidebar-bg"></div> */}
+          <div className="text-center mt-10 mb-12 z-10">
+            <div className="inline-flex mb-8">
+              <h1 className="text-white">Abyss</h1>
+              <h1 className="text-red-500">Media</h1>
+            </div>
+            <div>
+              <NavLink
+                exact
+                to="/"
+                className="sidebarContent"
+                activeClassName="activeContent"
+              >
+                <div className="inline-flex">
+                  <img className="h-8 pr-3" alt="Home" src={Home} />
+                  <a className=""> Home</a>
+                </div>
+              </NavLink>
+              <NavLink
+                to="/news"
+                className="sidebarContent"
+                activeClassName="activeContent"
+              >
+                <div className="inline-flex">
+                  <img className="h-8 pr-3" alt="Home" src={Home} />
+                  <a className=""> News</a>
+                </div>
+              </NavLink>
+              <NavLink
+                to="/69"
+                className="sidebarContent"
+                activeClassName="activeContent"
+              >
+                <div className="inline-flex">
+                  <img className="h-8 pr-3" alt="Home" src={Home} />
+                  <a className=""> Contact</a>
+                </div>
+              </NavLink>
+              <NavLink
+                to="/totallyfunctionalaboutpage"
+                className="sidebarContent"
+                activeClassName="activeContent"
+              >
+                <div className="inline-flex">
+                  <img className="h-8 pr-3" alt="Home" src={Home} />
+                  <a className=""> About</a>
+                </div>
+              </NavLink>
+              <NavLink
+                to="/account"
+                className="sidebarContent"
+                activeClassName="activeContent"
+              >
+                <div className="inline-flex">
+                  <img className="h-8 pr-3" alt="Character" src={Character} />
+                  <a className=""> Account</a>
+                </div>
+              </NavLink>
+            </div>
           </div>
-          <div>
-            <NavLink
-              exact
-              to="/"
-              className="sidebarContent"
-              activeClassName="activeContent"
-            >
-              <div className="inline-flex">
-                <img className="h-8 pr-3" alt="Home" src={Home} />
-                <a className=""> Home</a>
-              </div>
-            </NavLink>
-            <NavLink
-              to="/news"
-              className="sidebarContent"
-              activeClassName="activeContent"
-            >
-              <div className="inline-flex">
-                <img className="h-8 pr-3" alt="Home" src={Home} />
-                <a className=""> News</a>
-              </div>
-            </NavLink>
-            <NavLink
-              to="/69"
-              className="sidebarContent"
-              activeClassName="activeContent"
-            >
-              <div className="inline-flex">
-                <img className="h-8 pr-3" alt="Home" src={Home} />
-                <a className=""> Contact</a>
-              </div>
-            </NavLink>
-            <NavLink
-              to="/totallyfunctionalaboutpage"
-              className="sidebarContent"
-              activeClassName="activeContent"
-            >
-              <div className="inline-flex">
-                <img className="h-8 pr-3" alt="Home" src={Home} />
-                <a className=""> About</a>
-              </div>
-            </NavLink>
-            <NavLink
-              to="/account"
-              className="sidebarContent"
-              activeClassName="activeContent"
-            >
-              <div className="inline-flex">
-                <img className="h-8 pr-3" alt="Character" src={Character} />
-                <a className=""> Account</a>
-              </div>
-            </NavLink>
+        </div>
+        <div className="navbar p-0 justify-center">
+          <div className="text-center mt-2 mb-2 z-10">
+            <div>
+              <NavLink
+                exact
+                to="/"
+                className="sidebarContent m-0 items-stretch"
+                activeClassName="activeContent"
+              >
+                <div className="inline-flex">
+                  <img className="h-8 p-0" alt="Home" src={Home} />
+                </div>
+              </NavLink>
+              <NavLink
+                to="/navbarlmfao"
+                className="sidebarContent m-0 items-stretch"
+                activeClassName="activeContent"
+              >
+                <div className="inline-flex">
+                  <img className="h-8 p-0" alt="Home" src={Home} />
+                </div>
+              </NavLink>
+              <NavLink
+                to="/69"
+                className="sidebarContent m-0 items-stretch"
+                activeClassName="activeContent"
+              >
+                <div className="inline-flex">
+                  <img className="h-8 p-0" alt="Home" src={Home} />
+                </div>
+              </NavLink>
+              <NavLink
+                to="/pisser"
+                className="sidebarContent m-0 items-stretch"
+                activeClassName="activeContent"
+              >
+                <div className="inline-flex">
+                  <img className="h-8 p-0" alt="Home" src={Home} />
+                </div>
+              </NavLink>
+              <NavLink
+                to="/account"
+                className="sidebarContent m-0 items-stretch"
+                activeClassName="activeContent"
+              >
+                <div className="inline-flex">
+                  <img className="h-8 p-0" alt="Character" src={Character} />
+                </div>
+              </NavLink>
+            </div>
           </div>
         </div>
       </div>
-      <div className="navbar p-0 justify-center">
-        <div className="text-center mt-2 mb-2 z-10">
-          <div>
-            <NavLink
-              exact
-              to="/"
-              className="sidebarContent m-0 items-stretch"
-              activeClassName="activeContent"
-            >
-              <div className="inline-flex">
-                <img className="h-8 p-0" alt="Home" src={Home} />
-              </div>
-            </NavLink>
-            <NavLink
-              to="/navbarlmfao"
-              className="sidebarContent m-0 items-stretch"
-              activeClassName="activeContent"
-            >
-              <div className="inline-flex">
-                <img className="h-8 p-0" alt="Home" src={Home} />
-              </div>
-            </NavLink>
-            <NavLink
-              to="/69"
-              className="sidebarContent m-0 items-stretch"
-              activeClassName="activeContent"
-            >
-              <div className="inline-flex">
-                <img className="h-8 p-0" alt="Home" src={Home} />
-              </div>
-            </NavLink>
-            <NavLink
-              to="/pisser"
-              className="sidebarContent m-0 items-stretch"
-              activeClassName="activeContent"
-            >
-              <div className="inline-flex">
-                <img className="h-8 p-0" alt="Home" src={Home} />
-              </div>
-            </NavLink>
-            <NavLink
-              to="/account"
-              className="sidebarContent m-0 items-stretch"
-              activeClassName="activeContent"
-            >
-              <div className="inline-flex">
-                <img className="h-8 p-0" alt="Character" src={Character} />
-              </div>
-            </NavLink>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default NavBar;
--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,6 +2,7 @@
 import React from "react";
 // eslint-disable-next-line no-unused-vars
 import { Link } from "react-router-dom";
+import NotFoundImg from "./../img/404.png";
 
 class NotFoundPage extends React.Component {
   render() {
@@ -9,17 +10,15 @@ class NotFoundPage extends React.Component {
       <div className="content">
         <div className="pt-12">
           <img
-            className="w-60 mx-auto"
-            src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.pngmart.com%2Ffiles%2F11%2FFacepalm-PNG-Transparent-Image.png&f=1&nofb=1"
+            className="w-96 mx-auto"
+            src={NotFoundImg}
             alt="facepalm"
           />
-          <h1 className="text-center text-red-500">404</h1>
-          <p className="text-center">What. The. Actual. Hell.</p>
+          <h1 className="text-center pb-2">What. The. Actual. Hell.</h1>
           <p className="text-center font-bold">Did you just try to break my website!!</p>
-          <p className="text-center">
-            I am <strong>disappointed</strong>. But perhaps, I am not{" "}
-            <strong>surpised</strong> <a href="https://www.youtube.com/watch?time_continue=1&v=j5a0jTc9S10&feature=emb_title" target="_blank" rel="noreferrer">.</a>
-          </p>
+          <h2 className="text-center">
+            I am disappointed. But perhaps, I am not surpised <a href="https://www.youtube.com/watch?time_continue=1&v=j5a0jTc9S10&feature=emb_title" target="_blank" rel="noreferrer">.</a>
+          </h2>
         </div>
       </div>
     );
