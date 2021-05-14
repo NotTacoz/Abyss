18f395070dc53554bb59fde1bef1460577d50195
NotTacoz <tacozpvp@gmail.com>
2021-05-14 16:32:10 +0800

    Home.js now detects if you are signed in or not

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
+import "firebase/firestore";
 
-const app = firebase.initializeapp({
+const firebaseConfig = {
   apiKey: "AIzaSyCOETJBWJQ8dNQnJilsND5CoT79GBHKZUs",
   authDomain: "genshin-mains.firebaseapp.com",
   databaseURL: "https://genshin-mains-default-rtdb.firebaseio.com",
@@ -9,6 +10,8 @@ const app = firebase.initializeapp({
   messagingSenderId: "82953203784",
   appId: "1:82953203784:web:b825c166d63766c475287d",
   measurementId: "G-00DBVS0PXW",
-});
+};
 
-export default app;
+firebase.initializeapp(firebaseConfig);
+
+export default firebase;
