d15ff214ba5c7a57bb58fbaedd87216ce3f35b65
NotTacoz <tacozpvp@gmail.com>
2021-05-16 21:41:01 +0800

    Reverted

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,6 @@
 import firebase from "firebase/app";
-import "firebase/firestore";
 
-const firebaseConfig = {
+const app = firebase.initializeapp({
   apiKey: "AIzaSyCOETJBWJQ8dNQnJilsND5CoT79GBHKZUs",
   authDomain: "genshin-mains.firebaseapp.com",
   databaseURL: "https://genshin-mains-default-rtdb.firebaseio.com",
@@ -10,8 +9,6 @@ const firebaseConfig = {
   messagingSenderId: "82953203784",
   appId: "1:82953203784:web:b825c166d63766c475287d",
   measurementId: "G-00DBVS0PXW",
-};
+});
 
-firebase.initializeapp(firebaseConfig);
-
-export default firebase;
+export default app;
