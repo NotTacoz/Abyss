5e499f0171252adab3dbefa400ec2695eee9538e
NotTacoz <tacozpvp@gmail.com>
2021-06-06 22:57:19 +0800

    more additions cool

--- src/components/pages/Post.tsx
+++ src/components/pages/Post.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-import React from "react";
+import * as React from "react";
 import $ from "jquery";
 import { Helmet } from "react-helmet";
-import { RouteComponentProps } from "react-router";
+import { useParams } from "react-router-dom";
 
 import firebase from "firebase/app";
 import "firebase/auth";
@@ -13,6 +13,7 @@ import "firebase/analytics";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 
+import { UserGetData } from "../hooks/UserGetData";
 import { useGetData } from "../hooks/useGetData";
 
 const auth = firebase.auth();
@@ -20,34 +21,39 @@ const firestore = firebase.firestore();
 const analytics = firebase.analytics();
 const db = firebase.firestore();
 
-const FireStoreData = () => {
-  const [documents] = useGetData();
-};
-
-function Post() {
-  const [user] = useAuthState(auth);
-
-  return (
-    <div className="content">
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>Abyss | Post</title>
-      </Helmet>
-
-      <section>{user ? <Content /> : <SignIn />}</section>
-    </div>
-  );
-}
-
-function makeId(length) {
-  let result = [];
-  for (let i = 0; i < length; i++) {
-    result.push("0123456789"[Math.floor(Math.random() * 10)]);
-  }
-  return result.join("");
-}
-
 function toTime(date) {
+  let timestamp = date.toDate();
+  let currentDate = new Date();
+  if (
+    timestamp.getDate() === currentDate.getDate() &&
+    timestamp.getDay === currentDate.getDay
+  ) {
+    return `Today at ${
+      timestamp.getHours() % 12 === 0 ? 12 : timestamp.getHours() % 12
+    }:${timestamp.getMinutes().toString().padStart(2, "0")} ${
+      timestamp.getHours() > 11 ? "PM" : "AM"
+    }`;
+  } else {
+    return `${timestamp.getDate()} ${
+      [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ][timestamp.getMonth()]
+    } ${timestamp.getFullYear()}`;
+  }
+}
+
+function toExactTime(date) {
   let timestamp = date.toDate();
   return `${timestamp.getDate()} ${
     [
@@ -71,34 +77,94 @@ function toTime(date) {
   }`;
 }
 
-function Content() {
-  const [value, setValue] = React.useState("");
-  const getValue = (event) => {
-    setValue(event.target.value);
-  };
+function Post() {
+  const { id } = useParams();
+    const [value, setValue] = React.useState("");
 
-  const addValue = () => {
-    db.doc("values/" + makeId(10))
-      .set({
-        value: value,
-        user: auth.currentUser.uid,
-        time: new Date(),
-      })
-      .then(function () {
-        //console.log("Value successfully written!");
-      })
-      .catch(function (error) {
-        console.error("Error writing Value: ", error);
-      });
-  };
-  const [documents] = useGetData();
+    const uid = auth.currentUser.uid;
 
-  return <div className="">content</div>;
+    const [userInfo] = UserGetData();
+    const [documents] = useGetData();
+
+    var i;
+    if (userInfo !== undefined) {
+      for (i = 0; i in userInfo; i++) {
+        if (userInfo[i].id === uid) {
+          var SessionUserData = userInfo[i].value;
+        }
+      }
+    }
+
+    function getUserName(fuid) {
+      if (userInfo !== undefined) {
+        for (i = 0; i in userInfo; i++) {
+          if (userInfo[i].id === fuid) {
+            // console.log(userInfo[i].value)
+            return userInfo[i].value.username;
+          }
+        }
+      }
+    }
+    function getDisplayName(fuid) {
+      if (userInfo !== undefined) {
+        for (i = 0; i in userInfo; i++) {
+          if (userInfo[i].id === fuid) {
+            // console.log(userInfo[i].value)
+            return userInfo[i].value.displayName;
+          }
+        }
+      }
+    }
+    function getProfilePic(fuid) {
+      if (userInfo !== undefined) {
+        for (i = 0; i in userInfo; i++) {
+          if (userInfo[i].id === fuid) {
+            // console.log(userInfo[i].value)
+            return userInfo[i].value.photoUrl;
+          }
+        }
+      }
+    }
+
+  return (
+    <div className="content">
+      <h1>loading post {id}</h1>
+      <div>
+        {documents.map((documents) => (
+          <div key={documents.id}>
+            <div className="max-w-4xl break-all">
+              <div className="grid">
+                <div className="flex">
+                  <img
+                    className="w-12 mt-1 rounded-full"
+                    src={getProfilePic(documents.value.user)}
+                    alt="pfp"
+                  />
+                  <span className="pl-3 font-bold">
+                    {getDisplayName(documents.value.user)}{" "}
+                    <span className="font-light opacity-70">
+                      @{getUserName(documents.value.user)} ·{" "}
+                      {toTime(documents.value.time)}
+                    </span>
+                  </span>
+                </div>
+                <span className="ml-16 -mt-6 mb-2">
+                  {documents.value.value}
+                </span>
+                {/* <img
+                  draggable="true"
+                  alt="ExamplePicture"
+                  src="https://pbs.twimg.com/media/E28O61HUYAEtfbf?format=jpg&name=4096x4096"
+                  className="rounded-3xl max-w-sm ml-16"
+                /> */}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-function SignIn() {
-  window.location.href = "/account";
-  return (<div></div>);
-}
 
 export default Post;
